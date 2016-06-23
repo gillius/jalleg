26f7ee6c9f138574c4dd00c1387c0be490071568
Jason Winnebeck <gillius@gillius.org>
2016-06-22 23:35:43 -0400

    message box example in native dialogs

--- jalleg-examples/src/main/java/org/gillius/jalleg/example/NativeDialogs.java
+++ jalleg-examples/src/main/java/org/gillius/jalleg/example/NativeDialogs.java
@@ -3,6 +3,7 @@ package org.gillius.jalleg.example;
 import com.sun.jna.Pointer;
 import org.gillius.jalleg.binding.ALLEGRO_EVENT;
 import org.gillius.jalleg.binding.ALLEGRO_USER_EVENT;
+import org.gillius.jalleg.binding.AllegroLibrary;
 import org.gillius.jalleg.binding.AllegroLibrary.*;
 import org.gillius.jalleg.binding.NativeTextLogWriter;
 import org.gillius.jalleg.framework.AllegroAddon;
@@ -16,11 +17,15 @@ import static org.gillius.jalleg.binding.AllegroLibrary.*;
 
 public class NativeDialogs extends Game {
 
+	private static final short EXIT_ID = 1;
+	private static final short ALERT_ID = 2;
+	private ALLEGRO_TEXTLOG textlog;
+
 	@Override
 	protected void onAllegroStarted() throws Exception {
 		initAddons(AllegroAddon.NativeDialogs);
 
-		ALLEGRO_TEXTLOG textlog = al_open_native_text_log("Native Dialogs!", ALLEGRO_TEXTLOG_NO_CLOSE | ALLEGRO_TEXTLOG_MONOSPACE);
+		textlog = al_open_native_text_log("Native Dialogs!", ALLEGRO_TEXTLOG_NO_CLOSE | ALLEGRO_TEXTLOG_MONOSPACE);
 		jalleg_append_native_text_log(textlog, "123\n456\n");
 
 		PrintWriter pw = new PrintWriter(new NativeTextLogWriter(textlog), true);
@@ -28,7 +33,8 @@ public class NativeDialogs extends Game {
 		pw.println("Here be dragons!");
 
 		ALLEGRO_MENU menu = al_create_menu();
-		al_append_menu_item(menu, "Exit", (short) 1, 0, null, null);
+		al_append_menu_item(menu, "Exit", EXIT_ID, 0, null, null);
+		al_append_menu_item(menu, "Alert", ALERT_ID, 0, null, null);
 		al_set_display_menu(mainDisplay, menu);
 
 		al_register_event_source(eventQueue, al_get_default_menu_event_source());
@@ -46,8 +52,23 @@ public class NativeDialogs extends Game {
 	protected void onUnhandledEvent(ALLEGRO_EVENT event) {
 		if (event.type == ALLEGRO_EVENT_MENU_CLICK) {
 			ALLEGRO_USER_EVENT userEvent = event.asType(ALLEGRO_USER_EVENT.class);
-			if (Pointer.nativeValue(userEvent.data1) == 1)
+			long id = Pointer.nativeValue(userEvent.data1);
+			if (id == EXIT_ID) {
 				stop();
+
+			} else if (id == ALERT_ID) {
+				int button = al_show_native_message_box(mainDisplay, "Alert", "Test Alert",
+				                                        "This is a test of the alert system. Do you like it?", null,
+				                                        ALLEGRO_MESSAGEBOX_YES_NO);
+				String result;
+				switch (button) {
+					case 0: result = "Alert was closed without a button"; break;
+					case 1: result = "Yes was pressed"; break;
+					case 2: result = "No was pressed"; break;
+					default: result = "Unknown result"; break;
+				}
+				jalleg_append_native_text_log(textlog, result + "\n");
+			}
 		}
 	}
 
